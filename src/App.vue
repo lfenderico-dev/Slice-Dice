0faf9d87328fc30f87b33f9d4aa2c638b7fbfcb0
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-07-24 14:01:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavBar/>
+    <router-view/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <hero-section/>
+        <about-us/>
+        <menu-page/>
+        <review-section/>
+        <footer-section/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import HeroSection from '../components/HomePage/HeroSection.vue';
+import AboutUs from '../components/HomePage/AboutUs.vue';
+import MenuPage from '../components/HomePage/Menu.vue';
+import ReviewSection from '../components/HomePage/ReviewSection.vue';
+import FooterSection from '../components/HomePage/Footer.vue';
+</script>
+
+<style scoped>
+
+</style>
