1713b27c521170d77a92da54cf00f7511541e9dd
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-07-26 08:55:59 +0200

    First layout of the AboutSection

--- src/components/HomePage/AboutUs.vue
+++ src/components/HomePage/AboutUs.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
+    <div class="h-[100vh] flex flex-col">
+        <div class="flex flex-col">
+            <h1 class="text-center mt-8 text-2xl font-bold text-darkgray">About Us</h1>
+            <hr class="mx-auto w-15 h-2 bg-amber-500 border-0 rounded-sm">
 
+            <p class="text-center mx-3 my-5 bg-white shadow-2xl rounded-2xl p-5 text-sm">At Slice&Dice, we've been crafting authentic, delicious pizzas since 1980. We hand-toss our dough daily, use fresh ingredients, and bake every pizza to perfection.
+            Whether you're grabbing a quick bite or feeding the family, we're committed to serving you the perfect slice every time. We're proud to be part of this community and look forward to serving you!</p>
+
+            <div class="flex flex-row justify-center text-2xl my-8 text-darkgray gap-3">
+                <a href="https://www.instagram.com/"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="https://www.facebook.com/"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="https://www.tiktok.com/"><i class="fa-brands fa-tiktok"></i></a>
+                <a href="https://x.com/"><i class="fa-brands fa-square-x-twitter"></i></a>
+                <a href="https://www.linkedin.com/"><i class="fa-brands fa-square-linkedin"></i></a>
+                </div>
+        </div>
+        
+
+        <div class="flex justify-center">
+            <img class="rounded-xl w-80" src="/src/assets/images/waitress.jpg" alt="waitress">
+        </div>
+        
     </div>
 </template>
 
